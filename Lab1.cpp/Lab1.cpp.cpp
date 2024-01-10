1483fda4c3d981dc660132693ae9efb165fcf129
Joshua Eugene Tseitlin <jtseitli@purdue.edu>
2024-01-10 12:16:40 -0500

    Universe

--- Lab1.cpp/Lab1.cpp.cpp
+++ Lab1.cpp/Lab1.cpp.cpp
@@ -1,11 +1,11 @@
-// Lab1.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Lab1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello Other!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
